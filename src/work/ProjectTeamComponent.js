592d6fbafcc6d610bf3ac4f227779b7f653d6755
Shareek340 <nmshareek@gamil.com>
2023-10-17 11:19:40 +0530

    shareek

--- src/work/ProjectTeamComponent.js
+++ src/work/ProjectTeamComponent.js
@@ -1,20 +1,20 @@
 
-import './projectTeam.css'
+  import './projectTeam.css'
 
-function ProjectTeamComponent() {
-  return (
-    <div className={`project-team`}>
-      <p className={`project-team-work`}>Project Team </p>
-      <p className={`work-name`}>
-        Designer 1 Maxwell
-        <br />
-        Designer 2 Richardson
-        <br />
-        Designer 3 Salieri
-        <br />
-        Developer 1 Montego
-      </p>
-    </div>
-  );
-}
-export default ProjectTeamComponent
+  function ProjectTeamComponent() {
+    return (
+      <div className={`project-team`}>
+        <p className={`project-team-work`}>Project Team </p>
+        <p className={`work-name`}>
+          Designer 1 Maxwell
+          <br />
+          Designer 2 Richardson
+          <br />
+          Designer 3 Salieri
+          <br />
+          Developer 1 Montego
+        </p>
+      </div>
+    );
+  }
+  export default ProjectTeamComponent
